b95523ac57c4a54b960df05462802044ce485d90
Micah <Rus.Micah@gmail.com>
2020-06-04 15:30:57 +1000

    stuff

--- app.js
+++ app.js
@@ -2,40 +2,40 @@ const asciify = require('asciify-image');
 const readline = require('readline-sync');
 const fetch = require('node-fetch');
 
-  menu = ['View list', 'Search', 'Giraffe'],
-  userInput = readline.keyInSelect(menu, 'Which pokemon?');
+  // menu = ['View list', 'Search', 'Giraffe'],
+  // userInput = readline.keyInSelect(menu, 'Which pokemon?');
 
 const getPokemon = () => {
 fetch(`https://pokeapi.co/api/v2/pokemon`)
   .then(response => response.json())
-  .then(allPokemon => console.log(allPokemon.sprites.front_default));
+  .then(allPokemon => console.log(allPokemon.results.name));
 }
 
 getPokemon()
 
-  const pokemonNames = () => {
-    fetch('')
-  }
+  // const pokemonNames = () => {
+    
+  // }
 
-  switch (userInput) {
-    case 1
-    pokemonList = []
-  } 
+  // switch (userInput) {
+  //   case 1
+  //   pokemonList = []
+  // } 
 
 
-const options = {
-  fit:    'box',
-  width:  100,
-  height: 100
-}
+// const options = {
+//   fit:    'box',
+//   width:  100,
+//   height: 100
+// }
 
-const image = asciify(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${userInput}.png`, options, function (err, asciified) {
-  if (err) throw err;
+// const image = asciify(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${userInput}.png`, options, function (err, asciified) {
+//   if (err) throw err;
  
-  // Print to console
-  console.log(asciified);
-});
+//   // Print to console
+//   console.log(asciified);
+// });
 
-console.log(image);
+// console.log(image);
 
 
